4035bf8b7851e99cefd5e87b43c79b38e223f48e
anarthriagalumphed <galih22.mp@gmail.com>
2023-07-06 00:21:04 +0700

    upload movie app fix jadi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '606d417d8d600a1d4411e62ca9fb89bf',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '606d417d8d600a1d4411e62ca9fb89bf',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
